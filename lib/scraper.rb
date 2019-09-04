172beff109ad2bb8ddb69ee4c6e3b8b1e0cd9639
Patrick Harrison <jahman@bellatlantic.net>
2019-09-04 19:51:49 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,15 +1,15 @@
-require 'nokogiri'
-require 'open-uri'
+# require 'nokogiri'
+# require 'open-uri'
 
-html = open("https://flatironschool.com/")
+# html = open("https://flatironschool.com/")
 
-doc = Nokogiri::HTML(html)
-doc.css(".site-header__hero__headline")
+# doc = Nokogiri::HTML(html)
+# doc.css(".site-header__hero__headline")
 
-require 'nokogiri'
-require 'open-uri'
+# require 'nokogiri'
+# require 'open-uri'
  
-html = open("http://flatironschool.com/")
-doc = Nokogiri::HTML(html)
+# html = open("http://flatironschool.com/")
+# doc = Nokogiri::HTML(html)
  
-doc.css(".tout__label.heading.heading--level-4")
+# doc.css(".tout__label.heading.heading--level-4")
