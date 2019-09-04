d8c6ea9453b6d91840b7591d9d1908d2ab97950a
Patrick Harrison <jahman@bellatlantic.net>
2019-09-04 19:52:28 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -13,3 +13,16 @@
 # doc = Nokogiri::HTML(html)
  
 # doc.css(".tout__label.heading.heading--level-4")
+
+
+ require 'nokogiri'
+ require 'open-uri'
+ require "pry"
+
+#2. Save the site you’d like to scrape to a variable
+
+ site = "https://www.realtor.com/international/jm/rent/"
+
+
+ page = Nokogiri::HTML(open(site))
+  binding.pry
